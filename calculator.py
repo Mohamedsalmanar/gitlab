825b47c11b71272d27a291686946ca1c583e89de
muthu1809 <muthu1809@gmail.com>
2025-04-01 20:23:24 +0530

    April 1st notes

---
+++ calculator.py
@@ -0,0 +1,16 @@
+name = input()
+print("welcome to Python",name)
+
+#print என்பது ஒரு function. அதற்குள்ளே, ""இல் வார்த்தை கொடுத்திருக்கிறோம். அதில் ஒரு variableஉம் கொடுத்திருக்கிறோம். 
+
+# ~ --> tilde
+#````````` --> Back ticks  """"    '''''
+#pwd --> Print Working Directory
+# Folder ---> Directory
+#ls --> listing all files and folders in a directory
+#ls -ll
+
+#ctrl+l --> clear
+
+#What is Function in Python
+#What is Variable in Python
---
+++ first.py
@@ -0,0 +1,40 @@
+# Portable
+# Object Oriented programming language
+# Multi paradigm Language
+# Procedural Oriented - C
+# Modular Programming - Modula 3
+# Object Oriented Programming - C++
+
+#What is Function: செயல், வேலை, பணி 
+Set of Instructions with a name
+attending_class: 
+    1. Dress
+    2. Walking
+    3. Entering
+    4. Sitting
+    5. Listening
+    6. Taking Notes
+
+Naming Convention for Functions: 
+1. Function name should be meaningful. 
+    sit, stand, abcd, pqrs
+2. Function should not be Noun,should be verb. 
+3. Should end with ()
+4. Function names start with small letter. 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
