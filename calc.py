27bb67d31d98caa4942e1527e2ac2cea274ec937
muthu1809 <muthu1809@gmail.com>
2025-04-02 20:20:51 +0530

    apr 2nd notes

---
+++ calc.py
@@ -0,0 +1,34 @@
+def add(no1,no2):
+    print(no1+no2)  #Function Definition - வரையறை
+
+def subtract(no1,no2):
+    print(no1-no2)
+
+def multiply(no1,no2):
+    print(no1*no2)
+
+def divide(no1,no2):
+    print(no1/no2)
+
+def find_power(no1, no2):
+    print(no1 ** no2)    
+    pass
+
+
+add(10) #Arguments Passing
+subtract(100,50)
+multiply(2,3)
+
+#positional argument
+
+#def -> definition
+#add --> Function Name
+#no1, no2 --> Arguments / Parameters 
+#Arguments / parameters: 
+    #Inputs given to a function
+#eating(muttai_kuzhambu)
+
+#Function Calling
+
+#Task: 
+Types of Arguments 
