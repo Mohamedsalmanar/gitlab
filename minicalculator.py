ecead1a227896fc4ae3e7359e735d83c3b76f368
salman.ar <salmanwins@gmail.com>
2025-04-05 18:35:52 +0530

    minicalculatorv1

---
+++ minicalculator.py
@@ -0,0 +1,7 @@
+print("Welcome to mini Calculator, press enter")
+no1 = int(input("Enter the first no."))
+no2 = int(input("Enter the second no."))
+ops = input("what operation would you like to perform +, -, *, /")
+
+if ops == '+':
+    print("Result:",no1+no2)
