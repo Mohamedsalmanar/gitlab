de6b77f94f32e4eaa113fdbc9eeab337c05b426d
salman.ar <salmanwins@gmail.com>
2025-04-15 19:37:10 +0530

    apr15

---
+++ ReversingANumber.py
@@ -0,0 +1,7 @@
+number = 12345
+reversed_number = 0
+while number > 0:
+	reversed_number =  * 10 #5
+	number = number // 10 #1234
+	print(number)
+	print(reversed_number)
---
+++ countOfDigits.py
@@ -0,0 +1,7 @@
+#count of Digits
+number = empty
+count = 0
+while(number>0):
+	number = number // 10 #12345 = 1234
+	count = count +1
+print(count)
