01dfbebb0c2404cf5f74c9f63ed8fc3cb8d45598
Kuhanraja A R <kuhanrajaar@gmail.com>
2025-04-15 15:31:40 +0530

    Odd Number print

--- print.py
+++ print.py
@@ -1,23 +1,4 @@
-#1 2 3 4 5 6 7 8 9 10
-#RED BLUE
-
-no=1
-while no<=10:
-    if no%2==0:
-        print("Blue")
-    else:
-        print("Red")
-    no=no+1
-
-count = 1
-while count<=10:
-    if count%2==0 and count%4==0:
-        print("GREEN")
-    elif count%2==0:
-        print("BLUE")
-    else:
-        print("RED")
-    count+= 1
-
-
-
+no = 1234
+sum = 0
+if no > 0:
+    
---
+++ printwhile.py
@@ -0,0 +1,21 @@
+print(1)
+print(3)
+print(5)
+print(7)
+print(9)
+
+no = 1
+print(no)
+no=no+2
+print(no)
+no=no+2
+print(no)
+no=no+2
+print(no)
+no=no+2
+print(no)
+
+no = 1
+while no<=9:
+    print(no)
+    no=no+2
