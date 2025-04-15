753fc20f5c3e65649a90f011edb09bcf4fe057f0
Kuhanraja A R <kuhanrajaar@gmail.com>
2025-04-15 15:34:55 +0530

    reverse odd or even

---
+++ printwhileeven.py
@@ -0,0 +1,19 @@
+count=1
+while count<=10:
+    print(count)
+    count=count+2
+
+count=2
+while count<=10:
+    print(count)
+    count=count+2
+
+count=9
+while count>=0:
+    print(count)
+    count=count-2
+
+count=10
+while count>=0:
+    print(count)
+    count=count-2
