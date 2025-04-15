3a6ac81bd6d18f0c7cf4e69ef310c534a19d3d84
Kuhanraja A R <kuhanrajaar@gmail.com>
2025-04-15 15:17:11 +0530

    count.py

---
+++ count.py
@@ -0,0 +1,25 @@
+count = 1
+if count<=5: 
+    print(1)   
+count = count + 1 #count++ is not there in python
+if count<=5:
+    print(1)
+count = count + 1
+if count<=5:
+    print(1)
+count = count + 1
+if count<=5:
+    print(1)
+count = count + 1
+if count<=5:
+    print(1)
+count = count + 1
+if count<=5:
+    print(1)
+count = count + 1
+if count<=5:
+    print(1)
+count = count + 1
+if count<=5:
+    print(1)
+count = count + 1
