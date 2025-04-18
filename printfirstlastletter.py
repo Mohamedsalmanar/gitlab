836386d229629b06545030fe287014be5f5c42b1
Kuhanraja A R <kuhanrajaar@gmail.com>
2025-04-18 19:22:21 +0530

    index task 18 apr

---
+++ printfirstlastletter.py
@@ -0,0 +1,8 @@
+#sentence = input("enter name : ")
+#print("Your First letter is : ",sentence[0])
+#print("Your Last letter is : ",sentence[-1])
+
+sen = "salman"
+for every_letter in sen:
+	print(every_letter[0:1])     
+
