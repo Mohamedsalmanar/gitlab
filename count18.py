b0e9a02992415437e095465673203623ac1e4d09
mathan s <smathan9894@gmail.com>
2025-04-22 18:23:32 +0530

    hello

--- count18.py
+++ count18.py
@@ -2,26 +2,26 @@ def count_of_digits(no):
     count = 0
     while no>0:
         count+=1
-        no = no//10 #123
+        no = no//10 
     return count
 
 def find_power(base, power):
     result = 1
     while power>=1:
         result = result * base
-        power-=1 #power = power - 1
+        power-=1 
     return result
 
-def find_armstrong(no, power): #153
+def find_armstrong(no, power): 
     armstrong = 0
     while no>0:
-        rem = no%10     #3
+        rem = no%10     
         armstrong = armstrong + find_power(rem,power)
-        no = no//10 #123
+        no = no//10 
     return armstrong
     
 
-no = int(input("Enter no. ")) #153
+no = int(input("Enter no. ")) 
 count = count_of_digits(no)
 result = find_armstrong(no, count)
 
