3f47cd7ac5a381bace26e1d34efbed9afdd61d89
mathan s <smathan9894@gmail.com>
2025-04-30 19:00:46 +0530

    apr30.py

---
+++ apr30.py
@@ -0,0 +1,23 @@
+#len
+
+l1=[1,2,3,4,5]
+print(len(l1))
+
+#append
+
+l2=[9,8,7,6,5]
+l1.append(l2)
+print("appent",l1)
+
+
+#reverse true:
+
+l1 = [100,10,20,30]
+l1.sort(reverse=True)
+print(l1)
+
+#reverse false:
+
+l1 = [100,10,20,30]
+l1.sort(reverse=False)
+print(l1)
