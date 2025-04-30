6ecf5e7a67f12097003f4d7f351ba1cb7dc3c089
Kuhanraja A R <kuhanrajaar@gmail.com>
2025-04-30 19:48:54 +0530

    min.py

---
+++ min.py
@@ -0,0 +1,22 @@
+#l1 = [100,10,20,30]
+#print(min(l1))
+#print(max(l1))
+#print(sum(l1))
+
+l1 = ['muthusaravanan', 'suresh', 'rajesh']
+l1.sort()
+print(l1)
+
+l1 = ['muthusaravanan' , 'suresh' , 'rajesh']
+l2 = sorted(l1)
+print(l2)
+
+l1 = [10,20,30]
+l1.clear()
+print(l1)
+
+l1 = [10,20,30]
+l2 = l1
+print(l2)
+l1[0] = 111
+print(l2)
