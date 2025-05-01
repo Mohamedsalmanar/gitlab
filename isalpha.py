4c904802c1b0775c709d658ae0fad28b77339d8c
salman.ar <salmanwins@gmail.com>
2025-05-01 18:32:12 +0530

    apr1

--- isalpha.py
+++ isalpha.py
@@ -7,3 +7,13 @@ for letter in s:
 	else:
 		number = number + letter
 print(word+number)
+s = 'a1b2c3'
+letters = []
+for char in s:
+    if char.isalpha():
+        letters.append(char)
+print(letters)
+
+s='a1b2c3'
+letters = [char for char in s if char.isalpha()]
+print(letters)
