34f1884cc7a21a7d882d71a6562d64e8ad30150a
Kuhanraja A R <kuhanrajaar@gmail.com>
2025-05-01 18:25:43 +0530

    alpha

---
+++ alpha.py
@@ -0,0 +1,27 @@
+#s = 'a1b2c3'
+
+#alpha = ''
+#numbers = ''
+#for letter in s :
+#	if letter.isalpha():
+#		alpha = alpha + letter
+#	else:
+#		numbers = numbers + letter
+#print(alpha+numbers)
+
+#s = 'a1b2c3'
+#letters = []
+#for char in s:
+#	if char.isalpha():
+#		letters.append(char)
+#print(letters)
+
+#s='a1b2c3'
+#letters = [char for char in s if char.isalpha()]
+#print(letters)
+
+#print(ord('a'))
+#print(ord('b'))
+
+print(chr(ord('a')))
+print(chr(ord('a')+1))
--- list2.py
+++ list2.py
@@ -30,3 +30,13 @@
 #print(min(l1))
 #print(max(l1))
 #print(sum(l1))
+
+
+l1 = [10,20,30]
+l2 = l1
+print(l2)
+l1[0] = 111
+print(l2)
+
+print(id(l1))
+print(id(l2))
