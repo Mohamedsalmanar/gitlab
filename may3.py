b14a640f4a0292924608ab6d07781e38c68c84f6
mathan s <smathan9894@gmail.com>
2025-05-05 15:52:31 +0530

    allow

--- may3.py
+++ may3.py
@@ -1,4 +1,6 @@
-for row in range(1,7):
-        for col in range(row):
-                print(col+1,end='*')
-        print()
+for row in range(1,5):
+        for col in range(row-1):
+                print(col+1,end='')
+	for col in range(5-row):       
+	 print('*',end='')
+		print()
