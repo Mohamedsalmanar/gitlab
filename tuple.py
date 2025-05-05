8014de404785618bde319295a9778d959d6abdb6
Kuhanraja A R <kuhanrajaar@gmail.com>
2025-05-05 18:49:54 +0530

    may -5

---
+++ tuple.py
@@ -0,0 +1,40 @@
+#t1 = [10,20,30]
+#t2 = [10,20,30]
+#print(id(t1))
+#print(id(t2))
+
+#t = (10,20,30,40,50)
+#t2 = ()
+#print(type(t2))
+#t3 = (10)
+#print(type(t3))
+#t4 = (10,)
+#print(type(t4))
+
+#t = (10,20,30,40,50)
+#t[0] = 100
+#print(t)
+
+#t = (10,20,30,40,50)
+#print(len(t))
+#print(t[0:3])
+
+#t1 = (10,20,30)
+#t2 = (40,50,60)
+#t3 = t1 + t2
+#print(t3)
+#print(t1*2)
+#print(100 in t1)
+#print(100 not in t1)
+
+#t1 = (10,20,30)
+#t2 = (40,50,60)
+#print(id(t1))
+#print(id(t2))
+
+#t1 = (10,12,10,15,20,30,20,20,20)
+#print(t1.count(20))
+#print(t1.index(12))
+
+t1 = (10,5,8)
+print(sorted(t1,reverse = False))
