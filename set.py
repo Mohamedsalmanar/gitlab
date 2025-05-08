d8aef79009ce7ce31ccbdea78639c95bf01c81f0
salman.ar <salmanwins@gmail.com>
2025-05-08 18:31:53 +0530

    may5

---
+++ set.py
@@ -0,0 +1,13 @@
+set = {1, 1, 2, 2, 3, 4, 3}
+print(set)
+#print(set[0]) - Set object is not subscriptable
+set = {1.1, "salman", True, 123}
+print(set)
+
+s = range(1,6)
+#print(type(s))
+#print(s)
+
+t = (10, 10, 20, 30, 20)
+print(set(t))
+
