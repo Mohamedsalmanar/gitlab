52c5419cb5f876576006fec1762ae7030ade2329
salman.ar <salmanwins@gmail.com>
2025-05-08 19:31:15 +0530

    may8

---
+++ match_task.py
@@ -0,0 +1,6 @@
+match1 = {'sanju', 'virat', 'thilak', 'pandya'}
+match2 = {'sanju', 'rohit', 'thilak', 'ashwin'}
+
+#Both Match1 and Match2
+print(match1.union(match2))
+print(match1.intersection(match2))
