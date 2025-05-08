991d5e2c1a1ac102fe028df14cb6f9bb9657484f
salman.ar <salmanwins@gmail.com>
2025-05-08 20:05:16 +0530

    may8

---
+++ employee_dict.py
@@ -0,0 +1,9 @@
+employee = {'name':'raja','exp':5, 'city':'chennai'}
+print(employee)
+
+employee = {'name':'raja','exp':5, 'city':'chennai'}
+print(employee)
+
+print(employee['name'])
+employee['name'] = 'bala'
+print(employee)
