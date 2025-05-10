be1aa8a27c8c95c5e33ff6ee563112449ab38bdc
Kuhanraja A R <kuhanrajaar@gmail.com>
2025-05-10 18:35:56 +0530

    may-10

---
+++ palindrome.py
@@ -0,0 +1,32 @@
+#no = 121
+#while no>0:
+#	rem=no%10
+#	print(rem)
+#	no=no//10
+
+#total = 0
+#no = 121
+#while no>0:
+#	rem = no % 10
+#	total = total + rem
+#	no = no // 10
+#print(total)
+
+#sum = 0
+#no = 121
+#no2 = no
+#while no > 0:
+#	rem = no%10
+#	sum = (sum*10)+rem
+#	no = no//10
+#print(sum)
+
+#if no2 == sum:
+#	print('palindrome')
+#else :
+#	print('Not palindrome')
+
+name = 'amma'
+rev = name[::-1]
+if rev == name:
+	print("palindrome")
