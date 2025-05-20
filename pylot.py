8a9e1aa69fb21e5557f94821bc477fcb62d732f7
salman.ar <salmanwins@gmail.com>
2025-05-20 21:02:38 +0530

    may20

---
+++ pylot.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+y = [5, 10]
+y2 = [6, 12]
+plt.plot(y, label='Line 1')
+plt.plot(y2, label='Line 2')
+plt.xlabel('X-Axis')
+plt.ylabel('Y-Axis')
+plt.title('LINE GRAPH')
+plt.legend()
+plt.show()
